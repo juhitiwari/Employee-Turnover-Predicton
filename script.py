50757c58de60820fe47b40624d669781f571fceb
juhitiwari <tjuhi.321@gmail.com>
2018-05-15 13:12:18 +0530

    Hyperparameter tuning and best features

--- script.py
+++ script.py
@@ -13,6 +13,9 @@ from sklearn.tree import export_graphviz
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
+
 data = pd.read_csv("turnover.csv")
 data.salary = data.salary.astype('category')
 data.salary = data.salary.cat.reorder_categories(['low', 'medium', 'high'])
@@ -27,7 +30,7 @@ n_employees = len(data)
 target=data.churn
 features=data.drop('churn',axis=1)
 target_train, target_test, features_train, features_test = train_test_split(target,features,test_size=0.25,random_state=42)
-model = DecisionTreeClassifier(random_state=42,max_depth=7,class_weight='balanced')
+model = DecisionTreeClassifier(random_state=42,max_depth=5,class_weight='balanced')
 model.fit(features_train,target_train)
 print(model.score(features_train,target_train)*100)
 print(model.score(features_test,target_test)*100)
@@ -45,3 +48,30 @@ print(recall_score(target_test,prediction))
 prediction = model.predict(features_test)
 print(roc_auc_score(target_test, prediction))
 
+#Hyperparameter tuning
+print(cross_val_score(model,features,target,cv=10))
+depth = [i for i in range(5,21)]
+samples = [i for i in range(50,500,50)]
+parameters = dict(max_depth=depth,min_samples_leaf=samples)
+param_search = GridSearchCV(model, parameters)
+param_search.fit(features_train,target_train)
+print(param_search.best_params_)
+
+#important features
+feature_importances = model.feature_importances_
+feature_list = list(features)
+relative_importances = pd.DataFrame(index=feature_list, data=feature_importances, columns=["importance"])
+relative_importances.sort_values(by='importance', ascending=False)
+print(relative_importances.head())
+selected_features = relative_importances[relative_importances.importance>0.01]
+selected_list = selected_features.index
+features_train_selected = features_train[selected_list]
+features_test_selected = features_test[selected_list]
+
+#best model
+model_best = DecisionTreeClassifier(max_depth=8,min_samples_leaf=150,class_weight='balanced',random_state=42)
+model_best.fit(features_train_selected,target_train)
+prediction_best = model_best.predict(features_test_selected)
+print(model_best.score(features_test_selected,target_test)*100)
+print(recall_score(prediction_best,target_test)*100)
+print(roc_auc_score(prediction_best,target_test)*100)
